779af84574f81f275f9d5a887490f1097a902132
Owolabi sulaimon <owolabisulaimon90@gmail.com>
2022-10-18 07:53:20 +0200

    price adjust

--- test.py/MyShop.py
+++ test.py/MyShop.py
@@ -1,11 +1,11 @@
 print("""WELCOME TO MY SHOP!""")
 
-item   = input("What item are you purchasing?")
-price  = input("What is the price of " + item + "?")
-amount = input("how many" + item + "are you buying")
+item   = input("What item are you purchasing? ")
+price  = input("What is the price of " + item + "?" + " ")
+amount = input("How many" + item + " " + "are you buying? ")
 
 
 total = str(int(amount) * float(price))
 
 print("Added " + amount + " " + item + "(s) to shopping cart")
-print("Subtotat: $" + total)
+print("Subtotal: $" + total)
